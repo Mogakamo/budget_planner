33007a7eed494bf340c8920f5c29d4d172267d31
Mogakamo <mogaka.amo254@gmail.com>
2021-10-30 14:37:59 +0000

    Created the credit card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,47 @@
 <template>
   <div class="home">
-    <div class="sidebar bg-indigo-700">
+    <div class="sidebar bg-purple-700">
       <div
         class="
           rounded-lg
           shadow-xl
-          bg-blue-200
+          bg-purple-500
           white
-          content-center
+          flex flex-col
+          items-center
           pt-2
           pr-2
           pl-3
           pb-3
-          text-lg
           mt-4
           mr-4
           ml-4
         "
       >
-        <h1>$26,650</h1>
-        <span>current balance</span>
+        <h1 class="text-2xl font-bold text-white">$26,650</h1>
+        <span class="text-white">current balance</span>
+      </div>
+      <div class="menu">
+        <ul class="flex flex-col items-start pt-8 text-white">
+          <li class="p-3">
+            <i class="fa-solid fa-gauge-simple pr-3"></i><span>Dashboard</span>
+          </li>
+          <li class="p-3">
+            <i class="fa-solid fa-chart-bar pr-3"></i><span>Charts</span>
+          </li>
+          <li class="p-3">
+            <i class="fa-solid fa-calendar-days pr-3"></i><span>Calendar</span>
+          </li>
+          <li class="p-3">
+            <i class="fa-solid fa-envelope pr-3"></i><span>Messages</span>
+          </li>
+          <li class="p-3">
+            <i class="fa-solid fa-user pr-3"></i><span>User Profile</span>
+          </li>
+          <li class="p-3">
+            <i class="fa-solid fa-gear pr-3"></i><span>settings</span>
+          </li>
+        </ul>
       </div>
     </div>
     <div class="main bg-gray-200">Content</div>
@@ -39,7 +61,7 @@ export default {
 <style scoped>
 .home {
   display: grid;
-  grid-template-columns: 230px 1fr 300px;
+  grid-template-columns: 200px 1fr 300px;
   grid-template-rows: repeat(5, 1fr);
   min-height: 100vh;
   min-width: 100vw;
