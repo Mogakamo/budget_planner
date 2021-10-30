30302fbd15b1ffad9e6e0c2b694154cddb5a3f14
Mogakamo <mogaka.amo254@gmail.com>
2021-10-30 13:43:05 +0000

    working on the sidebar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,29 @@
 <template>
   <div class="home">
-    <div class="sidebar bg-blue-500">Sidebar</div>
-    <div class="main">Content</div>
-    <div class="aside">Side</div>
+    <div class="sidebar bg-indigo-700">
+      <div
+        class="
+          rounded-lg
+          shadow-xl
+          bg-blue-200
+          white
+          content-center
+          pt-2
+          pr-2
+          pl-3
+          pb-3
+          text-lg
+          mt-4
+          mr-4
+          ml-4
+        "
+      >
+        <h1>$26,650</h1>
+        <span>current balance</span>
+      </div>
+    </div>
+    <div class="main bg-gray-200">Content</div>
+    <div class="aside bg-yellow-200">Side</div>
   </div>
 </template>
 
@@ -20,26 +41,25 @@ export default {
   display: grid;
   grid-template-columns: 230px 1fr 300px;
   grid-template-rows: repeat(5, 1fr);
-  grid-column-gap: 3px;
-  grid-row-gap: 0px;
   min-height: 100vh;
   min-width: 100vw;
 }
 
 .sidebar {
-  border: 1px solid black;
+  border: transparent;
   grid-row-start: 1;
   grid-row-end: 6;
 }
 
 .main {
-  border: 1px solid black;
+  border: transparent;
   grid-row-start: 1;
   grid-row-end: 6;
+  border-top-left-radius: 50%b;
 }
 
 .aside {
-  border: 1px solid black;
+  border: transparent;
   grid-row-start: 1;
   grid-row-end: 6;
 }
