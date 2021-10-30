9bd6e4286b9fd8a06532da4e4eee62f027127b43
Mogakamo <mogaka.amo254@gmail.com>
2021-10-30 12:53:26 +0000

    finished with the layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,11 +18,29 @@ export default {
 <style scoped>
 .home {
   display: grid;
-  grid-template-columns: 150px 1fr 200px;
+  grid-template-columns: 230px 1fr 300px;
   grid-template-rows: repeat(5, 1fr);
   grid-column-gap: 3px;
   grid-row-gap: 0px;
   min-height: 100vh;
   min-width: 100vw;
+}
+
+.sidebar {
+  border: 1px solid black;
+  grid-row-start: 1;
+  grid-row-end: 6;
+}
+
+.main {
+  border: 1px solid black;
+  grid-row-start: 1;
+  grid-row-end: 6;
+}
+
+.aside {
+  border: 1px solid black;
+  grid-row-start: 1;
+  grid-row-end: 6;
 }
 </style>
